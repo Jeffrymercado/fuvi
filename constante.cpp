761529af12ee5a35111c05c4e23afdb6b749f222
Jeffry Mercado <jeffry25mercado@gmail.com>
2022-10-04 10:14:05 -0600

    Se modifico el archivo constante.cpp y se agrego Pitagoras.cpp

--- constante.cpp
+++ constante.cpp
@@ -1,3 +1,19 @@
 /*
 Nombre del Archivo: constante.cpp
+Instrucciones. Uso de las constantes
 */
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+const double pi = 3.141592;
+
+int main() {
+
+	double radio,area;
+	cout << "ingrese el radio del circulo: ";
+	cin >> radio;
+	area= pi * pow(radio,2);
+	cout << " El area de un circulo con radio " << radio << " es " << area;
+	return 0;
+}
