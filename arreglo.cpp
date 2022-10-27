8342b315a8417647846716f287965b1d1b99f6f2
Jeffry Mercado <jeffry25mercado@gmail.com>
2022-10-27 07:59:10 -0600

    Arreglo

---
+++ arreglo.cpp
@@ -0,0 +1,24 @@
+/*
+Nombre del Archivo : arreglos.cpp
+Autor : Jeffry Mercado
+Lugar : ITV
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	
+	double promedioNotas [10];
+	
+	promedioNotas[0] = 15.2;
+	promedioNotas[1] = 55.8;
+	
+	cout << promedioNotas[0] << endl;
+	cout << promedioNotas[1] << endl;
+	
+	
+	return 0;
+}
